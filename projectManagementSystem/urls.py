186dd644d345a17ed50d4cf306d12e9d57d455ac
PetyoKostakov <petyokostakov@gmail.com>
2015-08-09 13:24:41 +0300

    Refactor file structure

--- projectManagementSystem/urls.py
+++ projectManagementSystem/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import HelloWorldView
 
 urlpatterns = [
+    url(r'^$', HelloWorldView.as_view(), name='home'), # r'^$' expression that matches home url
     url(r'^admin/', include(admin.site.urls)),
 ]
---
+++ projectManagementSystem/views.py
@@ -0,0 +1,8 @@
+__author__ = 'Petyo'
+
+from django.views.generic.base import View
+from django.http import HttpResponse
+
+class HelloWorldView(View):
+    def get(self, request):
+        return HttpResponse("Hello, World!")
